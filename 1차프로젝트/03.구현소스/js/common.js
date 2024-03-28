8c3edb8dd7b98f4877c940f3a5b50cf786cca1a8
JaeYoonLee <a7750430@naver.com>
2024-03-28 18:06:14 +0900

    dd

--- 1차프로젝트/03.구현소스/js/common.js
+++
@@ -1,8 +0,0 @@
-// 날씨 지역 변경하기
-// 1-1. 이벤트 대상
-// 날씨 지역 
-// const ww = document.querySelectorAll('.weather-wrap dl');
-// const dbtn =document.querySelector('.button-wrap down');
-
-// console.log(dbtn,ww);
-
---
+++ 1차프로젝트/03.구현소스/js/slide.js
@@ -0,0 +1,17 @@
+// 전체 함수
+function slideFn(){
+
+    // 글자 애니메이션 아래->위
+    // 대상 .txt-banner
+
+    const stage = document.querySelector('.txt-banner');
+    console.log('대상', stage);
+
+    
+    
+    
+}////slideFn 함수
+
+
+
+slideFn();
