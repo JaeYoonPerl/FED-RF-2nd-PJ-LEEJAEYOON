1b9253a07f7f5453de5f07d93fd094e5515472f2
JaeYoonLee <a7750430@naver.com>
2024-03-29 18:10:16 +0900

    dd

--- 1차프로젝트/03.구현소스/js/slide.js
+++ 1차프로젝트/03.구현소스/js/slide.js
@@ -1,17 +1,19 @@
-// 전체 함수
-function slideFn(){
-
-    // 글자 애니메이션 아래->위
-    // 대상 .txt-banner
-
-    const stage = document.querySelector('.txt-banner');
-    console.log('대상', stage);
-
-    
-    
-    
-}////slideFn 함수
+// 글자 슬라이드 
+// 대상: .txt-banner
 
 
+// DOM 선택함수
+const qs = (x) => document.querySelector(x);
+const qsa = (x) => document.querySelectorAll(x);
 
-slideFn();
+// addEvent 함수
+// ele - 요소, evt - 이벤트, fn - 함수
+const addEvt = 
+(ele, evt, fn) => ele.addEventListener(evt, fn);
+
+// HTML태그 로딩후 loadFn함수 호출! ///
+addEvt(window,"DOMContentLoaded", loadFn);
+
+function loadFn(){
+    console.log("로딩완료!");
+}// loadFn
