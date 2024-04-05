8bb2c79591c3cda82cfa2a0c20a06dacd8b6145e
JaeYoonLee <a7750430@naver.com>
2024-04-05 17:56:27 +0900

    ddd

--- 1차프로젝트/03.구현소스/js/scroll_action.js
+++ 1차프로젝트/03.구현소스/js/scroll_action.js
@@ -37,7 +37,9 @@ function sideSC(){
 
 /* *************************on 넣기 **************************/
 // 스크롤 등장 기준 : 화면의 2/3
-const CRITERIA = (window.innerHeight / 2) * 1;
+const CRITERIA = (window.innerHeight / 3) * 2;
+// 스크롤 등장 기준 : 화면의 1/3
+const CRITERIA_OF =(window.innerHeight/3) * 1;
 
 // 스크롤 시
 
@@ -51,7 +53,10 @@ function addOn(ele){
     // 기준값보다 작을때 등장
     if (bcrVal < CRITERIA) ele.classList.add("on");
     // 기준값보다 크면 원상복귀(숨김-on빼기)
-    else ele.classList.remove("on");
+    else if(bcrVal > CRITERIA ) ele.classList.remove("on");
+
+    
+   
 
 }
 
