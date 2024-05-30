b93e56b5f3c9616cde241b3da9a661e0881319c8
JaeYoonLee <a7750430@naver.com>
2024-05-30 18:15:23 +0900

    das

---
+++ 2차프로젝트/03.구현소스/nexon-app/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
+export default function MainComponent() {
+    return (
+        <>
+            <h1>넥슨컴퍼니</h1>
+        </>
+    );
+}
+
+// 컴포넌트 출력
+// 먼저 root 객체 만들기
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+// 출력하기
+root.render(<MainComponent />);
