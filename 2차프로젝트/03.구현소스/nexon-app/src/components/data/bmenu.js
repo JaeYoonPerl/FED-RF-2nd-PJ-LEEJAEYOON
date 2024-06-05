abd1d347a74fc899dde861d33bc5879043e44b6f
JaeYoonLee <a7750430@naver.com>
2024-06-05 18:13:12 +0900

    Create bmenu.js

---
+++ 2차프로젝트/03.구현소스/nexon-app/src/components/data/bmenu.js
@@ -0,0 +1,28 @@
+// 하단 링크 정보
+
+export const bmData = [
+    {
+        txt:"개인정보처리방침",
+        link:"https://company.nexon.com/ko/privacy-policy",
+    },
+    {
+        txt:"CONTACT US",
+        link:"https://company.nexon.com/ko/contact-us",
+    },
+    {
+        txt:"LOCATIONS",
+        link:"",
+    },
+    {
+        txt:"윤리경영",
+        link:"https://company.nexon.com/ko/ethical-management",
+    },
+    {
+        txt:"공고",
+        link:"https://company.nexon.com/ko/announcement",
+    },
+    {
+        txt:"IR",
+        link:"https://ir.nexon.co.jp/kr/",
+    },
+];
