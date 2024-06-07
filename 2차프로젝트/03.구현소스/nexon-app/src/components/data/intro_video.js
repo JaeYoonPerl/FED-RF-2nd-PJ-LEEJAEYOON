0c8060e30c1b9b3e91d980df8d631e8a5a6b3e55
JaeYoonLee <a7750430@naver.com>
2024-06-07 18:11:55 +0900

    d

---
+++ 2차프로젝트/03.구현소스/nexon-app/src/components/data/intro_video.js
@@ -0,0 +1,13 @@
+// 이미지 경로 정보
+export const ivideo = {
+
+    //  메인영상 
+    mainv: "/videos/main_video.mp4",
+    // company영상
+    companyv: "/videos/company_video.mp4",
+    // business영상1
+    businessv01: "/videos/business_video01.mp4",
+    // business영상2
+    businessv02: "/videos/business_video02.mp4",
+    
+};
--- 2차프로젝트/03.구현소스/nexon-app/src/components/pages/Main.jsx
+++ 2차프로젝트/03.구현소스/nexon-app/src/components/pages/Main.jsx
@@ -1,11 +1,17 @@
+import React from "react";
+import { ivideo } from "../data/intro_video";
+
 // 메인 페이지 컴포넌트 ///
 
 export default function Main(){
     // 코드 리턴구역 ////
     return(
-        <>
+        <div className ="firstinro">
+           {/* 1. 인트로 구역 */}
+           <video className="mainV" autoPlay loop muted>
+                <source src={ivideo.mainv} type='video/mp4' />
+            </video>
            
-           <h1>첫페이지</h1>
-        </>
+        </div>
     );
 } /////// Main /////
