e1f1b3074be8e896db8216c597a8b918bdc60fcd
JaeYoonLee <a7750430@naver.com>
2024-06-13 18:15:35 +0900

    dd

--- 2차프로젝트/03.구현소스/nexon-app/src/components/layout/FooterArea.jsx
+++ 2차프로젝트/03.구현소스/nexon-app/src/components/layout/FooterArea.jsx
@@ -4,8 +4,9 @@ import { menu } from "../data/gnb";
 
 // 하단 메뉴 데이터 불러오기
 import { bmData } from "../data/bmenu";
+
 // 콤보박스 데이터 불러오기
-import comboData from "../data/combo_data.js";
+import {comboData} from "../data/combo_data.js";
 
 // 하단영역 CSS 불러오기
 import LogoFoot from "../modules/Logo_foot";
@@ -13,34 +14,42 @@ import LogoFoot from "../modules/Logo_foot";
 // 하단 영역 CSS
 import "../../css/footer_area.scss";
 
-export default function FooterArea(){
-    
+export default function FooterArea() {
     // 코드 리턴구역 ////
-    return(
+    return (
         <footer className="info">
             <ul>
                 <li>
                     {/* 하단 링크 박스 */}
                     <ol className="bmenu">
-                    {
-                        bmData.map((v,i)=>
-                        <li key={i}>
-                            <a href={v.link} target="_blank">
-                                {v.txt.toUpperCase()}
-                            </a>
-                        </li>
-                        )
-                    }
+                        {bmData.map((v, i) => (
+                            <li key={i}>
+                                <a href={v.link} target="_blank">
+                                    {v.txt.toUpperCase()}
+                                </a>
+                            </li>
+                        ))}
                     </ol>
                 </li>
                 <li className="copyright">© NEXON Korea Corporation All Rights Reserved</li>
                 {/* 하단로고 컴포넌트 */}
                 <li>
-                    <LogoFoot logoStyle="bottom"/>
+                    <LogoFoot logoStyle="bottom" />
                 </li>
             </ul>
-            {/* 콤보박스 바인딩 */}
-            
+            {/* 콤보박스 */}
+            <div className="combo-box">
+                
+                <ul>
+                    {comboData.map((v, i) => (
+                        <li>
+                            <a href={v.brandLink} target="_blank">
+                                {v.brand}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </footer>
     );
 } /////// FooterArea /////
---
+++ 2차프로젝트/03.구현소스/nexon-app/src/components/modules/ComboBox.jsx
@@ -0,0 +1,15 @@
+// 하단 콤보박스
+import React from 'react';
+// 콤보박스 데이터 불러오기
+import comboData from "../data/combo_data.js";
+
+function ComboBox(props) {
+     
+    return (
+        <div>
+            <button>Fa</button>
+        </div>
+    );
+}
+
+export default ComboBox;
