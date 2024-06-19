695f3471f9ac97ef05d418cedb68dcca1c1a8bbd
JaeYoonLee <a7750430@naver.com>
2024-06-19 16:19:38 +0900

    dd

--- 2차프로젝트/03.구현소스/nexon-app/src/components/modules/GameList.jsx
+++ 2차프로젝트/03.구현소스/nexon-app/src/components/modules/GameList.jsx
@@ -13,29 +13,32 @@ import "../../css/game_list.scss";
 
 function GameList(props) {
     return (
-        <ul className="gArea">
-            {gameData.map((v, i) => (
-                <li key={i} className="gBox">
-                    <Link to="/" className="gListLink">
-                        <ol className="gList">
-                            <li className="gImg">
-                                <img src={v.대표이미지} alt={v.게임명} />
-                            </li>
-                            <div className="gtxtBox">
-                                <li className="gName">{v.게임명}</li>
-                                <li className="genre">{v.장르}</li>
-                                <li className="platform">
-                                    {v.플랫폼[0] == "PC" ? <FontAwesomeIcon icon={faDesktop} /> : ""}
-                                    {v.플랫폼[1] == "모바일" ? <FontAwesomeIcon icon={faMobileScreen} /> : ""}
-                                    {v.플랫폼[2] == "콘솔" ? <FontAwesomeIcon icon={faGamepad} /> : ""}
+        <section className="gListSec">
+            <h2>전체게임</h2>
+            <div className="filter"></div>
+            <ul className="gArea">
+                {gameData.map((v, i) => (
+                    <li key={i} className="gBox">
+                        <Link to="/" className="gListLink">
+                            <ol className="gList">
+                                <li className="gImg">
+                                    <img src={v.대표이미지} alt={v.게임명} />
                                 </li>
-                            <li></li>
-                            </div>
-                        </ol>
-                    </Link>
-                </li>
-            ))}
-        </ul>
+                                <div className="gtxtBox">
+                                    <li className="gName">{v.게임명}</li>
+                                    <li className="genre">{v.장르}</li>
+                                    <li className="platform">
+                                        {v.플랫폼[0] == "PC" ? <FontAwesomeIcon icon={faDesktop} /> : ""}
+                                        {v.플랫폼[1] == "모바일" ? <FontAwesomeIcon icon={faMobileScreen} /> : ""}
+                                        {v.플랫폼[2] == "콘솔" ? <FontAwesomeIcon icon={faGamepad} /> : ""}
+                                    </li>
+                                </div>
+                            </ol>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </section>
     );
 }
 
--- 2차프로젝트/03.구현소스/nexon-app/src/components/plugin/SwiperList.jsx
+++ 2차프로젝트/03.구현소스/nexon-app/src/components/plugin/SwiperList.jsx
@@ -1,6 +1,7 @@
 // 메인 페이지 스와이퍼 플러그인 컴포넌트
 
 import React, { useEffect, useLayoutEffect } from "react";
+import { Link } from "react-router-dom";
 // Import Swiper React components
 import { Swiper, SwiperSlide, useSwiper, useSwiperSlide } from "swiper/react";
 // 제이쿼리 불러오기
@@ -26,87 +27,96 @@ import { swData } from "../data/swiper_data";
 // import { Navigation } from "swiper/modules";
 
 export function SwiperList({ catName }) {
-  // 선택데이터 변수할당
-  const selData = swData[catName];
+    // 선택데이터 변수할당
+    const selData = swData[catName];
 
-  return (
-    <>
-      <Swiper
-        // slidesPerView={4.5}
-        spaceBetween={20}
-        navigation={true}
-        /* 사용할 모듈을 여기에 적용시킨다 */
-        // modules={[Navigation]}
-        breakpoints={{
-          200: {
-            slidesPerView: 1,
-          },
-          500: {
-            slidesPerView: 2,
-          },
-          1000: {
-            slidesPerView: 3,
-          },
-          1200: {
-            slidesPerView: 4.5,
-          },
-        }}
-        className="mySwiper"
-      >
-        {selData.map((v, i) => (
-          <SwiperSlide key={i}>
-            <section className="sw-inbox">
-              {/* 타이틀 박스 */}
-              <div className="mnews-tit">
-                <h4>{v.date}</h4>
-                <h3>{v.tit}</h3>
-              </div>
-              {/* 이미지박스 */}
-              <div className="mnews-img">
-                <img src={v.isrc} alt={v.tit} />
-                
-              </div>
-            </section>
-          </SwiperSlide>
-        ))}
-        <SlidePrevButton />
-        <SlideNextButton />
-      </Swiper>
-    </>
-  );
+    return (
+        <>
+            <Swiper
+                // slidesPerView={4.5}
+                spaceBetween={20}
+                navigation={true}
+                /* 사용할 모듈을 여기에 적용시킨다 */
+                // modules={[Navigation]}
+                breakpoints={{
+                    200: {
+                        slidesPerView: 1,
+                    },
+                    500: {
+                        slidesPerView: 2,
+                    },
+                    1000: {
+                        slidesPerView: 3,
+                    },
+                    1200: {
+                        slidesPerView: 4.5,
+                    },
+                }}
+                className="mySwiper"
+            >
+                {selData.map(
+                    (v, i) =>
+                        Number(v.idx) <= 10 && (
+                            <SwiperSlide key={i}>
+                                <Link
+                                  to="/detail" style={{ textDecoration: "none"}}
+                                  // state로 데이터 넘기기
+
+                                  >
+                                    <section className="sw-inbox">
+                                        {/* 타이틀 박스 */}
+                                        <div className="mnews-tit">
+                                            <h4>{v.date}</h4>
+                                            <h3>{v.tit}</h3>
+                                        </div>
+                                        {/* 이미지박스 */}
+                                        <div className="mnews-img">
+                                            <img src={v.isrc} alt={v.tit} />
+                                        </div>
+                                    </section>
+                                </Link>
+                            </SwiperSlide>
+                        )
+                )}
+                <SlidePrevButton />
+                <SlideNextButton />
+            </Swiper>
+        </>
+    );
 }
 
 function SlideNextButton() {
-  const swiper = useSwiper();
+    const swiper = useSwiper();
 
-  return (
-    <button className="swiper-button-next" onClick={
-        (e) => {
-            swiper.slideNext();
-            // console.log(swiper.activeIndex);
-            if(swiper.activeIndex==6) e.currentTarget.disabled = true;
-            else e.currentTarget.previousElementSibling.disabled = false;
-        }
-        }>
-      <FontAwesomeIcon icon={faChevronRight} />
-    </button>
-  );
+    return (
+        <button
+            className="swiper-button-next"
+            onClick={(e) => {
+                swiper.slideNext();
+                // console.log(swiper.activeIndex);
+                if (swiper.activeIndex == 6) e.currentTarget.disabled = true;
+                else e.currentTarget.previousElementSibling.disabled = false;
+            }}
+        >
+            <FontAwesomeIcon icon={faChevronRight} />
+        </button>
+    );
 }
 function SlidePrevButton() {
-  const swiper = useSwiper();
+    const swiper = useSwiper();
 
-  return (
-    <button
-      className="swiper-button-prev" 
-      onClick={(e) => {
-        swiper.slidePrev();
-        
-        // console.log(swiper.activeIndex);
-        if(swiper.activeIndex==0) e.currentTarget.disabled = true;
-        else e.currentTarget.nextElementSibling.disabled = false;
-    }}
-    >
-        <FontAwesomeIcon icon={faChevronLeft} />
-    </button>
-  );
+    return (
+        <button
+            className="swiper-button-prev"
+            onClick={(e) => {
+                swiper.slidePrev();
+
+                // console.log(swiper.activeIndex);
+                if (swiper.activeIndex == 0) e.currentTarget.disabled = true;
+                else e.currentTarget.nextElementSibling.disabled = false;
+            }}
+        >
+            <FontAwesomeIcon icon={faChevronLeft} />
+        </button>
+    );
 }
